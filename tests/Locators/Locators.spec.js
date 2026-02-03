ae16238368b9f9530c2c94c34486f7857995f51e
Deepak Sharma <deepssharma0312@gmail.com>
2026-02-03 23:16:49 +0000

    GetByRole HAndon

--- tests/Locators/Locators.spec.js
+++ tests/Locators/Locators.spec.js
@@ -1,14 +1,13 @@
 //getByRole
-
 import { test, expect } from "@playwright/test"
-test("Locators", async ({ page }) => {
+test("Testing locators", async ({ page }) => {
     await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html")
     await expect(page).toHaveTitle("Automation Testing Practice: PlaywrightPractice")
-    const locator = page.getByRole("button", { name: "Primary Action" })
-    await expect(locator).toHaveText("Primary Action")
-    await locator.click()
-    await expect(locator).toBeVisible()
-    await page.getByRole("textbox", {}).toBeVisible
-    await page.getByRole("checkbox", { name: " Accept terms" }).check()
-    await expect(page.getByRole("button", { name: "Div with button role" })).toBeVisible()
+    await page.getByRole("button", { name: "Primary Action" }).click()
+    await expect(page.getByRole("button", { name: "Primary Action" })).toBeVisible()
+    await expect(page.getByRole("textbox", { name: "username" })).toBeVisible()
+    await page.getByRole("checkbox", { name: "Accept terms" }).check()
+    await expect(page.getByRole("checkbox", { name: "Accept terms" })).toBeChecked()
+    await page.getByRole("menuitem", { name: "Home" }).click()
+    await expect(page.getByRole("menuitem", { name: "Home" })).toBeVisible()
 })
