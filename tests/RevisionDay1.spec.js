43f38ee6621a6417c4ac26b28a08dda4176abcc8
Deepak Sharma <deepssharma0312@gmail.com>
2026-02-28 22:30:59 +0000

    Revision Day1 - 28/02/2026

---
+++ tests/RevisionDay1.spec.js
@@ -0,0 +1,44 @@
+import { test, expect } from "@playwright/test"
+test("Revision for Locatos and Basic actions in Playwright", async ({ page }) => {
+    await page.goto("https://testautomationpractice.blogspot.com/p/playwrightpractice.html");
+    await expect(page).toHaveTitle("Automation Testing Practice: PlaywrightPractice");
+
+    //getByRole
+    await page.getByRole("button", { name: "Primary Action" }).click()
+    await expect(page.getByRole("button", { name: "Primary Action" })).toBeVisible()
+
+    //getByText
+    await page.getByText("Submit Form").click()
+    await expect(page.getByText("List item 1")).toBeVisible()
+
+    //getByLabel
+    await page.getByLabel("Email Address:").fill("Deepak")
+    await expect(page.getByLabel("Email Address:")).toHaveValue("Deepak");
+
+    //getByPalaceholder
+    await page.getByPlaceholder("Enter your full name").fill("Deepak Sharma")
+    await expect(page.getByPlaceholder("Enter your full name")).toHaveValue("Deepak Sharma");
+
+    //getByAltText
+    await expect(page.getByAltText("logo image", { exact: true })).toBeVisible()
+
+    //getByTitle
+    await expect(page.getByTitle("Home page link")).toBeVisible()
+    await expect(page.getByTitle("Click to save your changes", { exact: true })).toBeVisible()
+
+    //getByTestId
+    await page.getByTestId("edit-profile-btn").click()
+    await expect(page.getByTestId("edit-profile-btn")).toBeVisible()
+
+    //CSS
+    await page.locator("css=button[role='button']").click()
+    // await expect(page.locator("css=button[text()='Primary Action']")).toBeVisible()
+
+    //XPATH
+    await page.locator("xpath=//button[@aria-pressed='false']").click()
+    await expect(page.locator("xpath=//button[text()='Primary Action']")).toBeVisible()
+
+    //shadow DOM
+    await expect(page.locator("#shadow_host").getByText("Mobiles")).toBeVisible()
+    await page.locator("#shadow_host").getByRole("textbox").fill("Deepak Sharma - Shadow DOM")
+})
--- tests/firstTest.spec.js
+++
@@ -1,5 +0,0 @@
-import { test, expect } from "@playwright/test"
-test("My first test in Playwright", async ({ page }) => {
-    await page.goto("https://www.nirankari.org/")
-    await expect(page).toHaveTitle("Sant Nirankari Mission")
-})
